d3de10a6e297198968506abc09d82031724d80fa
cyrem <m.juerg@online.de>
2016-10-06 14:58:48 +0200

    creation

---
+++ README.md
@@ -0,0 +1,7 @@
+# uebungmle
+
+A Clojure app to ... well, that part is up to you.
+
+## Usage
+
+FIXME
---
+++ src/uebungmle/core.clj
@@ -0,0 +1,8 @@
+(ns uebungmle.core
+  (:gen-class))
+
+
+
+
+(defn -main []
+  (println "Hello, World!"))

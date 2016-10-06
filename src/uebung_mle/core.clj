4b21caed29ffe4327e475d3c41b0c6d333a7fed8
cyrem <m.juerg@online.de>
2016-10-06 17:47:54 +0200

    getCities, create map

--- src/uebung_mle/core.clj
+++ src/uebung_mle/core.clj
@@ -8,17 +8,17 @@
 (defn getCities [m cityCol cityRow]
   (nth m (+ cityCol (* cols cityRow))))
 
-
-
 (defn create-map [nr]
-  (vec (map (fn [_](rand-int 25)) (range (* nr nr)))))
-
-
+  (vec (map (fn [x] 
+              (if (=  (quot x cols) (mod x cols) )
+                0
+                (rand-int 25)))
+            (range (* nr nr)))))
 
 
 (defn -main []
-
-  (println "Hello, World!"))
   
+  (println "Hello, World!"))
+
 
 

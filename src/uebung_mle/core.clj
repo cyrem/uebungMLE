a7e33d383a02e36ed3b94fee3d2dc5897f6bc7f3
cyrem <m.juerg@online.de>
2016-10-09 14:38:00 +0200

    objective FUnc

--- src/uebung_mle/core.clj
+++ src/uebung_mle/core.clj
@@ -1,23 +1,53 @@
 (ns uebungmle.core
   (:gen-class))
 
-
-(def cols 100)
-(def rows 100)
+(def cities 100)
 
 (defn getCities [m cityCol cityRow]
-  (nth m (+ cityCol (* cols cityRow))))
+  (if (<= cityCol cityRow)
+    (nth m (+ cityCol (* cities cityRow)))
+    (nth m (+ cityRow (* cities cityCol)))))
 
-(defn create-map [nr]
-  (vec (map (fn [x] 
-              (if (=  (quot x cols) (mod x cols) )
-                0
-                (rand-int 25)))
-            (range (* nr nr)))))
+(defn create-map []
+  (mapv (fn [x] 
+          (if (=  (quot x cities) (mod x cities) )
+            0
+            (rand-int 25)))
+        (range (* cities cities))))
+
+(defn getFitnessValue [v mapping]
+  (+ (loop [i 0
+            erg 0]
+       (if (< i 99)
+         (recur (inc i) 
+                (+ erg 
+                   (getCities mapping (nth v i) (nth v (inc i)))))
+         erg))
+     (getCities mapping (first v) (last v))))
+
+(def startVec (vec (range 100)))
+(def curMap (create-map))
+
+(defn swap [v i1 i2] 
+  (assoc v i2 (v i1) i1 (v i2)))
+
+(def theta 200)
+
+(defn optimize [v mapping]
+  (let [nrA (rand-int (count v))
+        nrB (rand-int (count v))
+        currentFitness (getFitnessValue v mapping)
+        newVec (swap v nrA nrB)
+        newFitness (getFitnessValue newVec mapping)]
+    (println currentFitness " -> " newFitness)
+    (if (< newFitness currentFitness)   
+      (if (< newFitness theta)
+        newVec
+        (recur newVec mapping))
+      (recur v mapping))))
 
 
 (defn -main []
-  
   (println "Hello, World!"))
 
 

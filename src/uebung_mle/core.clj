51747d294946e37b21dd401d4d65203fa6880a9a
cyrem <m.juerg@online.de>
2016-10-11 17:35:24 +0200

    sim annealing

--- src/uebung_mle/core.clj
+++ src/uebung_mle/core.clj
@@ -9,7 +9,7 @@
     (nth m (+ cityRow (* cities cityCol)))))
 
 (defn create-map []
-  (mapv (fn [x] 
+  (mapv (fn [x]
           (if (=  (quot x cities) (mod x cities) )
             0
             (rand-int 25)))
@@ -23,7 +23,7 @@
                 (+ erg 
                    (getCities mapping (nth v i) (nth v (inc i)))))
          erg))
-     (getCities mapping (first v) (last v))))
+     (getCities mapping (nth v 0) (nth v (dec(count v))))))
 
 (def startVec (vec (range 100)))
 (def curMap (create-map))
@@ -31,21 +31,41 @@
 (defn swap [v i1 i2] 
   (assoc v i2 (v i1) i1 (v i2)))
 
-(def theta 200)
 
-(defn optimize [v mapping]
+;(hillClimbing startVec curMap 100000)
+(defn hillClimbing [v mapping nr]
   (let [nrA (rand-int (count v))
         nrB (rand-int (count v))
         currentFitness (getFitnessValue v mapping)
         newVec (swap v nrA nrB)
         newFitness (getFitnessValue newVec mapping)]
-    (println currentFitness " -> " newFitness)
-    (if (< newFitness currentFitness)   
-      (if (< newFitness theta)
-        newVec
-        (recur newVec mapping))
-      (recur v mapping))))
+    ;(println nr currentFitness " -> " newFitness)
+    (if (= nr 0) 
+      (do (println currentFitness)
+        newVec)
+      (if (< newFitness currentFitness)
+        (recur newVec mapping (dec nr))
+        (recur v mapping (dec nr))))))
 
+(defn weightedFitness [fY fX t]
+  (Math/exp (- (*(/ (- fY fX) t)1000))))
+
+(def eps 5)
+
+;(simAnneal startVec 100000)
+(defn simAnneal [v t]
+  (let [nrA (rand-int (count v))
+        nrB (rand-int (count v))
+        currentFitness (getFitnessValue v curMap)
+        newVec (swap v nrA nrB)
+        newFitness (getFitnessValue newVec curMap)]
+    (println currentFitness t)
+    (if (< t eps)
+        newVec
+      (if (or (< newFitness currentFitness)  (< (rand) (weightedFitness newFitness currentFitness t)) )
+        (recur newVec (- t eps))
+        (recur v (- t eps))))
+    ))
 
 (defn -main []
   (println "Hello, World!"))
